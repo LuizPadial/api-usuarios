0c915bbbd785db9e724f34580f02e5068ed6096f
luizPadial <luizpadial10@gmail.com>
2024-07-02 20:27:56 -0300

    feat: criado endpoints e arquivo simulando bd

---
+++ src/dados/dados.js
@@ -0,0 +1,28 @@
+const users =[
+    {
+        id:1,
+        name: "Fulano",
+        email: "fulano@email.com",
+        password: "fu12334"
+    },
+    {
+        id:2,
+        name: "Beltrano",
+        email: "berltrano@email.com",
+        password: "be12334"
+    },
+    {
+        id:3,
+        name: "Ciclano",
+        email: "ciclano@email.com",
+        password: "ci12334"
+    },
+    {
+        id:4,
+        name: "Satoro",
+        email: "satoro@email.com",
+        password: "sa12334"
+    },
+];
+
+module.exports = users;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 const express = require('express')
+const users =require('./dados/dados')
 const app = express()
 
-app.get('/login', (req, res) => {
+app.get('/users', (req, res) => {
     console.log("Dentro do endpoint")
-    res.send("Server Rodando")
+    res.status(200)
+    res.send(users)
+});
+
+app.get('/users/:id', (req, res) => {
+    const { id } = req.params;
+
+    if (Number(id) >users.length){
+        res.send("Usuário não cadastrado.")
+    }
+
+    const user = users.find(user => user.id === Number(id)) 
+    console.log(user)
+    res.status(200)
+    res.send(user)
 });
 
 app.listen(3000, ()=>{

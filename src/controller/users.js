71797005158a1bafd68f072f65eeb42f1afed9c8
luizPadial <luizpadial10@gmail.com>
2024-07-03 21:54:26 -0300

    feat: adicionado metodo delete

--- src/controller/users.js
+++ src/controller/users.js
@@ -43,9 +43,18 @@ const userUpdate = (req, res) => {
     }
 }
 
+const userDelete = (req, res) => {
+    const { id } =req.params;
+    const indexUser = users.findIndex(user => user.id === Number(id))
+    users.splice(indexUser, 1)
+    res.status(200).send("User Deleted")
+    
+}
+
 module.exports = {
     usersList,
     userGet,
     userCreate,
-    userUpdate
+    userUpdate,
+    userDelete
 }
